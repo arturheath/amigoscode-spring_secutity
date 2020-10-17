975943dcd5a9107b92ec461cfa740b6e58dd25c4
Artur Shamrin <ashamrin@utrace.ru>
2020-10-17 17:21:01 +0300

    init commit: no logic yet

---
+++ src/main/java/com/example/amigoscode_srpingsecirity/AmigoscodeSrpingsecirityApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscode_srpingsecirity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeSrpingsecirityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeSrpingsecirityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amigoscode_srpingsecirity/AmigoscodeSrpingsecirityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscode_srpingsecirity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeSrpingsecirityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
